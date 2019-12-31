eeccb8b5a1ab3e1eee8ffb23dd50037ced143113
MostlyArmless <mike.j.thiem@gmail.com>
2019-12-30 23:25:16 -0700

    Added input validation in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@
 // npm start -- 2
 
 let problemNumber = parseInt( process.argv[2] );
+
+if ( problemNumber < 2 || problemNumber > 25 )
+{
+    console.error( `Invalid problem number ${problemNumber}` );
+    process.exit();
+}
+
 console.log( `Running problemNumber ${problemNumber}...` );
-
-
 const problem = require( `./ProblemsAndInputs/problem${problemNumber}.js` );
-
-console.log( `DONE.` );
