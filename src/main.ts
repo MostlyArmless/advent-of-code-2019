893f50a645484b26ddc2fd926c9e23d056194cc7
Mike Thiem <mike.j.thiem@gmail.com>
2020-11-26 11:47:39 -0700

    Fixed main.ts so you can now run an individual problem from the command line.
    e.g. to run problem3 from the command line:
    npm start -- 3

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
 // CLI syntax example to run problem 2
-// npm start -- 2
+// npm start -- 3
+
+import { measureExecutionTime } from "./tools";
 
 let problemNumber = parseInt( process.argv[2] );
+// let problemNumber = 3; // Uncomment this line instead of the line above in order to run from the debugger. Or pass the args via launch.json.
 
 if ( problemNumber < 2 || problemNumber > 25 )
 {
@@ -12,4 +15,10 @@ if ( problemNumber < 2 || problemNumber > 25 )
 }
 
 console.log( `Running problemNumber ${problemNumber}...` );
-const problem = require( `./ProblemsAndInputs/problem${problemNumber}.js` );
+const problemModule = require( `D:/Dev/advent-of-code-2019/build/src/ProblemsAndInputs/problem${problemNumber}.js` );
+const exportedFunctionNames = Object.keys( problemModule );
+
+exportedFunctionNames.forEach( func =>
+{
+    measureExecutionTime( problemModule[func] );
+} );
